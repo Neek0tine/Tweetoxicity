fbb5651e77f09e173ef5ff68b1318da929c3307b
Neek0tine <nicholas.juan.kalvin-2020@stmm.unair.ac.id>
2021-12-12 16:37:29 +0700

    Added everything else, exception catcher, and some other stuffs. v1.1

--- src/scrapper.py
+++ src/scrapper.py
@@ -1,49 +1,86 @@
-import os
-import time
-import tweepy
-import pandas as pd
 from os import getenv
+import pandas as pd
+import tweepy
+from time import sleep
 
 
-consumer_key = getenv('tweepy_consumer_key')
-consumer_secret = getenv('tweepy_consumer_secret')
-access_token = getenv('tweepy_access_token')
-access_token_secret = getenv('tweepy_access_secret')
-auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
-auth.set_access_token(access_token, access_token_secret)
-api = tweepy.API(auth, wait_on_rate_limit=True)
-print(tweepy.__version__)
+def _init():
+    try:
+        print('Authenticating ...')
+        consumer_key = getenv('tweepy_consumer_key')
+        print("Consumer key [OK]")
+        consumer_secret = getenv('tweepy_consumer_secret')
+        print("Consumer secret [OK]")
+        access_token = getenv('tweepy_access_token')
+        print("API Access token [OK]")
+        access_token_secret = getenv('tweepy_access_secret')
+        print("API Secret [OK]")
+        print(" -- Authenticated --")
+    except Exception as e:
+        print(f"API Access tokens are not inside systen environment variables. Please contact developer! ({e.args}")
 
-def search():
-    user = ''
-    search_user = api.search_users(input("Username: "), count=10)
+    try:
+        auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+        auth.set_access_token(access_token, access_token_secret)
+        _api = tweepy.API(auth, wait_on_rate_limit=True)
+        api = _api
+    except Exception as e:
+        print(f"Fail to authenticate user. Please try again later or contact developers! ({e.args})")
 
-    if len(search_user) > 1:
-        for index, users in enumerate(search_user, start=1):
-            print(index, users.screen_name)
-        search()
+    return api
 
+
+def get_user_stats(query=None):
+    _user = None
+
+    if query is None:
+        _query = input("Search : ")
     else:
-        for users in search_user:
-            print("Username :", users.name)
-            print("Display name :", users.screen_name)
-            print("Location :", users.location)
-            print("Bio :", users.description)
-            print("Followers :", users.followers_count)
-            print("Following :", users.friends_count)
-            print("Account birthdate :", users.created_at)
-            print()
-            user = users
+        _query = query
+
+    _search_result = api.search_users(_query, count=1)
+
+    _user = _search_result[0]
+    print("\n"+"-"*40,f"\nShowing profile of {_user.screen_name}")
+    print("Display name :", _user.name)
+    print("Location :", _user.location)
+    print("Bio :", _user.description)
+    print("Followers :", _user.followers_count)
+    print("Following :", _user.friends_count)
+    print("Account birthdate :", _user.created_at, "\n"+"-"*40, "\n")
+    user = _user
     return user
 
-_count = 150
-try:
-    user = search()
-    tweets = tweepy.Cursor(api.user_timeline, user_id=user.id).items(_count)
-    tweets_list = [[tweet.created_at, tweet.text] for tweet in tweets]
-    tweets_df = pd.DataFrame(tweets_list)
-    print(tweets_df)
+def get_user_tweets(user=None):
+    _count = 150
 
-except BaseException as e:
-    print('failed on_status,', str(e))
-    time.sleep(3)
+    if user is None:
+        _user = get_user_stats()
+
+    try:
+        tweets = tweepy.Cursor(api.user_timeline, user_id=_user.id).items(_count)
+        tweets_list = [[tweet.created_at, tweet.text] for tweet in tweets]
+        tweets_df = pd.DataFrame(tweets_list)
+        print(tweets_df)
+        tweets_df.to_csv(f'Tweets of {_user.screen_name}.csv', index=False)
+
+    except BaseException as e:
+        print('failed on_status,', str(e))
+        sleep(3)
+        get_user_tweets()
+
+
+if __name__== "__main__":
+    api = _init()
+    print("┌" + "─" * 56 + "┐")
+    print("""  _____              _                                   
+    |_   _| _ _ ___ ___| |_ ___ ___ ___ ___ ___ ___ ___ ___ 
+      | || | | | -_| -_|  _|_ -|  _|  _| .'| . | . | -_|  _|
+      |_||_____|___|___|_| |___|___|_| |__,|  _|  _|___|_|  
+                                           |_| |_|          """)
+    print("  Tweetscrapper version 1.1")
+    print('  Tweepy version', tweepy.__version__)
+    print("  https://github.com/Neek0tine/Tweetoxicity")
+    print("└" + "─" * 56 + "┘")
+
+    get_user_tweets()
