ae8a4c794c3fea530ff206cf4621a9ac7f3cad51
Fathurrahman Syarief <fathurrahman.syarief-2020@stmm.unair.ac.id>
2021-12-14 20:37:35 +0700

    create sentiment per tweets prototype

--- src/scripts/routes.py
+++ src/scripts/routes.py
@@ -1,6 +1,14 @@
 from scripts import app
 from flask import render_template
+import pandas as pd
 
 @app.route('/')
 def home_page():
     return render_template('base.html')
+
+@app.route('/table')
+def table_page():
+    textfile = pd.read_csv('./data/elonmusk_tweets.csv')
+    items = [(a,b,c) for a,b,c in zip(textfile['original text'], textfile['sentiment'], textfile['confidence'])]
+    
+    return render_template('table.html', items=items)
