2067cd84b1bbe15edce79b7c8f0b05696ae5b1f4
Fathurrahman Syarief <fathurrahman.syarief-2020@stmm.unair.ac.id>
2021-12-22 16:34:15 +0700

    add stuff

--- src/scripts/routes.py
+++ src/scripts/routes.py
@@ -1,14 +1,27 @@
+from flask.helpers import url_for
 from scripts import app
-from flask import render_template
-import pandas as pd
+from flask import render_template, request, redirect
+from scripts.tweepy_api import get_user_tweets
+from scripts.preprocess import tweetoxicity
 
-@app.route('/')
+@app.route("/", methods=["POST", "GET"])
+@app.route("/home", methods=["POST", "GET"])
 def home_page():
-    return render_template('base.html')
+    if request.method == "POST":
+        username = request.form.get("username")
+        return redirect(url_for("result_page", user=username))
+    else:
+        return render_template('home.html')
 
-@app.route('/table')
-def table_page():
-    textfile = pd.read_csv('./models/data/elonmusk_tweets.csv')
-    items = [(a,b,c) for a,b,c in zip(textfile['original text'], textfile['sentiment'], textfile['confidence'])]
-    
-    return render_template('table.html', items=items)
+username = []
+@app.route("/home/result/<user>")
+def result_page(user):
+    username.append(user)
+    return render_template('result.html')
+
+@app.route("/home/result/details")
+def resultdetails_page():
+    tweetscrap = get_user_tweets(username)
+    tweetmodels = tweetoxicity(tweetscrap)
+    datas = [(a,b,c) for a,b,c in zip(tweetmodels['original text'], tweetmodels['sentiment'], tweetmodels['confidence'])]
+    return render_template('resultdetails.html', items=datas)
