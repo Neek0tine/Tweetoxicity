0a0db6771e4b5c6ba0ebf141801d81cea3aa845a
Neek0tine <nicholas.juan.kalvin-2020@stmm.unair.ac.id>
2022-01-05 01:37:15 +0700

    - Profile preview - Fixes

--- src/scripts/routes.py
+++ src/scripts/routes.py
@@ -1,7 +1,9 @@
+import pandas as pd
 from flask import render_template, request, redirect, Response
 from scripts.preprocess import models_script
 from scripts.tweepy_api import tweetox
 from scripts.wordcld import WORDCLOUD
+import requests
 # from .errors import defaultHandler
 from flask.helpers import send_from_directory, url_for
 from scripts import app
@@ -12,16 +14,20 @@ from PIL import Image
 
 
 user = []
+userent = []
+tweets = []
+tweets_sentiment = []
 
 
 @app.route("/", methods=["POST", "GET"])
 @app.route("/home", methods=["POST", "GET"])
 def home_page():
     if request.method == "POST":
-        username = request.form.get("username")
-        user.append(str(username))
-        return redirect(url_for("result_page", user=username))
+        _username = request.form.get("username")
+        user.append(str(_username))
+        return redirect(url_for("result_page"))
     else:
+        user.clear()
         return render_template('home.html')
 
 
@@ -30,23 +36,21 @@ def about_page():
     return render_template('about.html')
 
 
-tweets = []
-tweets_sentiment = []
+@app.route("/result")
+def result_page():
+    _user = "".join(user)
+    print(f'[+] Getting tweets of {_user}')
+    _tweetscrap = None
+
+    if str(_user).startswith('@'):
+        global userent
+        _tweetscrap, userent = (tweetox(_user).get_user_tweets())
 
 
-@app.route("/home/result/<user>")
-def result_page(user):
-    print(f'[+] Getting tweets of {user}')
-    _tweetscrap = ''
-
-    if str(user).startswith('@'):
-
-        _tweetscrap = (tweetox(user).get_user_tweets())[0]
-
-        # try:
-        #     _tweetscrap = (tweetox(user).get_user_tweets())[0]
-        # except Exception as e:
-        #     raise defaultHandler
+        # # try:
+        # #     _tweetscrap = (tweetox(user).get_user_tweets())[0]
+        # # except Exception as e:
+        # #     raise defaultHandler
 
         if _tweetscrap is None:
             print('[!] Could not find user timeline')
@@ -55,7 +59,7 @@ def result_page(user):
             pass
 
     else:
-        _tweetscrap = tweetox(user).get_tweets()
+        _tweetscrap = tweetox(_user).get_tweets()
         if _tweetscrap is None:
             print('[!] Could not find any tweets related to tag!')
             _tweetscrap = None
@@ -64,7 +68,7 @@ def result_page(user):
 
     if _tweetscrap.empty:
         print('[!] User/Tag doesnt have tweets')
-        return render_template('tweets_null.html', username=user)
+        return render_template('tweets_null.html', username=_user)
     else:
         _tweetmodels, _accountsentiment, _sentimentcount = models_script(_tweetscrap)
         tweets.append(_tweetmodels)
@@ -76,26 +80,80 @@ def result_page(user):
         else:
             _color = 'rgba(255, 99, 71, 0.78)'
 
-        return render_template('result.html', username=user, account_sentiment=_accountsentiment, color=_color)
+        return render_template('result.html', username=_user, account_sentiment=_accountsentiment, color=_color)
 
 
-@app.route("/home/result/details")
+@app.route("/result/details")
 def resultdetails_page():
     Items = []
-    for tweet in tweets:
-        # dataframe
-        Items = [(a, b, c) for a, b, c in zip(tweet['original text'], tweet['sentiment'], tweet['confidence'])]
-        
-        # Wordcloud
-        WORDCLOUD(tweet)
-        
-    data = []
-    for twt in tweets_sentiment:
-        POSITIVE = int(twt.query("final_sentiment == 'POSITIVE'")["sentiment"])
-        NEGATIVE = int(twt.query("final_sentiment == 'NEGATIVE'")["sentiment"])
-        data = {'Sentiment': 'Count', 'Positive': POSITIVE, 'Negative': NEGATIVE}
+    if userent is None:
+        for tweet in tweets:
+            # dataframe
+            Items = [(a, b, c) for a, b, c in zip(tweet['original text'], tweet['sentiment'], tweet['confidence'])]
 
-    return render_template('result_details.html', items=Items, dashboardPie=data)
+            # Wordcloud
+            WORDCLOUD(tweet)
+
+        data = []
+        for twt in tweets_sentiment:
+            POSITIVE = int(twt.query("final_sentiment == 'POSITIVE'")["sentiment"])
+            NEGATIVE = int(twt.query("final_sentiment == 'NEGATIVE'")["sentiment"])
+            data = {'Sentiment': 'Count', 'Positive': POSITIVE, 'Negative': NEGATIVE}
+
+        return render_template('result_details.html', items=Items, dashboardPie=data)
+
+    else:
+        _profile_pic = str(userent.profile_image_url)
+        _screen_name = userent.screen_name
+        _name = userent.name
+        _location = userent.location
+        _description = userent.description
+
+        def human_format(num):
+            num = float('{:.3g}'.format(num))
+            magnitude = 0
+            while abs(num) >= 1000:
+                magnitude += 1
+                num /= 1000.0
+            return '{}{}'.format('{:f}'.format(num).rstrip('0').rstrip('.'), ['', 'K', 'M', 'B', 'T'][magnitude])
+
+        _followers = human_format(userent.followers_count)
+        _friends = human_format(userent.friends_count)
+        _birth = userent.created_at
+
+        with open('scripts/static/bootstrap/img/pp.jpg', 'wb') as handle:
+            response = requests.get(_profile_pic, stream=True)
+            if not response.ok:
+                print(response)
+            for block in response.iter_content(1024):
+                if not block:
+                    break
+                handle.write(block)
+
+        for tweet in tweets:
+            # dataframe
+            Items = [(a, b, c) for a, b, c in zip(tweet['original text'], tweet['sentiment'], tweet['confidence'])]
+
+            # Wordcloud
+            WORDCLOUD(tweet)
+
+        data = []
+        for twt in tweets_sentiment:
+            POSITIVE = int(twt.query("final_sentiment == 'POSITIVE'")["sentiment"])
+            NEGATIVE = int(twt.query("final_sentiment == 'NEGATIVE'")["sentiment"])
+            data = {'Sentiment': 'Count', 'Positive': POSITIVE, 'Negative': NEGATIVE}
+
+        return render_template('user_details.html',
+                               items=Items,
+                               dashboardPie=data,
+                               _profile_pic=_profile_pic,
+                               _screen_name=_screen_name,
+                               _name=_name,
+                               _location=_location,
+                               _description=_description,
+                               _followers=_followers,
+                               _friends=_friends,
+                               _birth=_birth)
 
 
 @app.route("/download")
--- src/scripts/wordcld.py
+++ src/scripts/wordcld.py
@@ -1,7 +1,9 @@
 from numpy import NaN
 from wordcloud import WordCloud
 from PIL import Image
+import random
 import pandas as pd
+
 
 def readCSV():
     path = r'C:\Users\fathu\Downloads\data (17).csv'
@@ -10,15 +12,24 @@ def readCSV():
     df.dropna(inplace=True)
     return df
 
+
+def grey_color_func(word, font_size, position, orientation, random_state=None,
+                    **kwargs):
+    return "hsl(0, 0%%, %d%%)" % random.randint(90, 100)
+
+
 def WORDCLOUD(df):
     file = " ".join(str(v) for v in df['clean text'])
     
     WC = WordCloud(
+        font_path='scripts/static/bootstrap/font/AdobeCleanLight.otf',
+        collocations=False,
         max_words=len(df),
-        width=600,
-        height=400,
-        background_color="white"
+        width=560,
+        height=315,
+        background_color="rgba(255, 255, 255, 0)", mode="RGBA"
     ).generate(file)
+    WC.recolor(color_func=grey_color_func, random_state=3)
     
     return WC.to_file('scripts/static/bootstrap/img/wordcloud.png')
 
