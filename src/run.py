dee1ee8a65d6dc1e5aaa38d1bb4480e6a646644a
austhopia <71798396+fathur-rs@users.noreply.github.com>
2022-02-08 20:35:11 +0700

    Update run.py

--- src/run.py
+++ src/run.py
@@ -5,7 +5,7 @@ import os
 from scripts.errors import defaultHandler
 
 if __name__ == '__main__':
-    app.run(host="0.0.0.0", port=int(os.environ.get('PORT', 33507)), debug=True, threaded=True)
+    app.run(host="0.0.0.0", port=int(os.environ.get('PORT', 80)), debug=True, threaded=True)
     app.config['TRAP_HTTP_EXCEPTIONS'] = True
     app.register_error_handler(Exception, defaultHandler)
     app.logger.basicConfig(level=logging.ERROR)
